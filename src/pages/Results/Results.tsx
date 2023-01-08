292887eb83ccd418f2693a8067161a07427f1bd2
Thiago López <thiagolopezalderete@hotmail.com>
2023-01-08 19:49:12 -0300

    refactor(Results): results.tsx file refactored

--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { useEffect, useState } from "react";
 import { getResponses } from "@/database";
 import { ChartCountries, ChartCountriesFallback } from "@/components";
@@ -6,20 +7,20 @@ import { FormResponse } from "@/types";
 export default function Results(): JSX.Element {
   const [results, setResults] = useState([] as FormResponse[]);
   const [loading, setLoading] = useState(true);
+
   const handleResponses = async (): Promise<void> => {
-    const responses = await getResponses();
-    const data = responses.docs.map((doc) => doc.data());
-    setResults(data as FormResponse[]);
+    try {
+      const responses = await getResponses();
+      const data = responses.docs.map((doc) => doc.data()) as FormResponse[];
+      setResults(data);
+      setLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   useEffect(() => {
-    handleResponses()
-      .then(() => {
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    handleResponses();
   }, []);
 
   return (
